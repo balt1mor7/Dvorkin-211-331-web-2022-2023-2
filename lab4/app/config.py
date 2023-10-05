4b00e68e856942b9756f5903f71977c054621bdf
Дмитрий <91124368+balt1mor7@users.noreply.github.com>
2023-10-05 17:31:41 +0300

    Delete lab4 directory

--- lab4/app/config.py
+++
@@ -1,6 +0,0 @@
-SECRET_KEY = '592fa8db13393d0198b433a3683e48aa2ae68c00f7a1d9e1723c6a4a563ccc9e'
-
-MYSQL_USER = 'std_1844_2023_lab4'
-MYSQL_PASSWORD = '12345678'
-MYSQL_HOST = 'std-mysql.ist.mospolytech.ru'
-MYSQL_DATABASE = 'std_1844_2023_lab4'
